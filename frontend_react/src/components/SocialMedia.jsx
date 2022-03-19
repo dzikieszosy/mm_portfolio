30c607b5aa3824cba4746127b75a09aa7a750344
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2022-03-19 11:47:02 +0100

    sanity work updated, work section completed

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-import { BsInstagram } from "react-icons/bs";
+import { BsInstagram, BsGithub } from "react-icons/bs";
 import { SiGmail } from "react-icons/si";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <SiGmail />
+      <a href="mailto:michal1986maciorowski@gmail.com">
+        <SiGmail />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://www.instagram.com/dzikieszosy/" target="_blank">
+        <BsInstagram />
+      </a>
+    </div>
+    <div>
+      <a href="https://github.com/dzikieszosy" target="_blank">
+        <BsGithub />
+      </a>
     </div>
   </div>
 );
